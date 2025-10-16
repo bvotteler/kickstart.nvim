3b7a181202fe653e0d7b36262f579311cf5d4b77
Brendon Votteler <bvotteler@users.noreply.github.com>
2025-10-16 08:54:14 +0200

    initial vimwiki setup

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,26 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'vimwiki/vimwiki',
+    --event = "BufEnter *.md",
+    lazy = false, -- load asap
+    keys = { '<leader>ww', '<leader>wt' },
+    init = function()
+      vim.g.vimwiki_folding = ''
+      vim.g.vimwiki_list = {
+        {
+          path = '~/.local/share/wiki',
+          syntax = 'markdown',
+          ext = '.md',
+        },
+      }
+      vim.g.vimwiki_ext2syntax = {
+        ['.md'] = 'markdown',
+        ['.markdown'] = 'markdown',
+        ['.mdown'] = 'markdown',
+      }
+    end,
+  },
+}
